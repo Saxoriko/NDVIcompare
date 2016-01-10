7cee245b7b035ea26c49304d1a2031d52ab9957c
wschuc002 <william.schuch@wur.nl>
2016-01-10 20:42:52 +0100

    Added description

--- R/ndviCalc.R
+++ R/ndviCalc.R
@@ -1,4 +1,13 @@
+# Student: William Schuch & Rik van Berkum
+# Team: Geodetic Engineers of Utrecht
+# Student#: 901120-751-050 & 931112-059-020
+# Institute: Wageningen University and Research
+# Course: Geo-scripting (GRS-33806)
+# Date: 2016-01-08
+# Week 1, Lesson 5: Intro to raster
+
 ndviCalc <- function(x) {
+	# calculates NDVI
 	ndvi <- (x[[2]] - x[[1]]) / (x[[2]] + x[[1]])
 	return(ndvi)
 }
--- R/ndviFilt.R
+++ R/ndviFilt.R
@@ -1,5 +1,15 @@
+# Student: William Schuch & Rik van Berkum
+# Team: Geodetic Engineers of Utrecht
+# Student#: 901120-751-050 & 931112-059-020
+# Institute: Wageningen University and Research
+# Course: Geo-scripting (GRS-33806)
+# Date: 2016-01-08
+# Week 1, Lesson 5: Intro to raster
+
 ndviFilt <- function(x,y) {
+	# Filters out: clear water pixel, cloud shadow, snow and cloud 
 	x[y > 0.9] <- NA
+	# Filters out: NDVI outliers 
 	x[x >1] <- NA
 	x[x <-1] <- NA
 	return(x)
